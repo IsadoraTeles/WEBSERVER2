aac7cab9095adba49038c0bad8875fbb6c18ff52
isadorateles <isadoratelesdecastro@gmail.com>
2021-08-26 11:40:34 +0200

    v8 test museeonaute inspired

--- public/sketch.js
+++ public/sketch.js
@@ -2,11 +2,20 @@ var socket = io();
 
 var renderer;
 
-var AccX, AccY, AccZ;
-var xpos = 200;
-var ypos = 200;
-var zpos = 0;
-var acc = true;
+// Position Variables
+let myPosX = 0;
+let myPosY = 0;
+
+// Speed - Velocity
+let myVelX = 0;
+let myVelY = 0;
+
+// Acceleration
+let myAccX = 0;
+let myAccY = 0;
+
+let vMultiplier = 0.007;
+let bMultiplier = 0.6;
 
 function setup() 
 {
@@ -16,49 +25,44 @@ function setup()
 
 	// set canvas size
 	renderer = createCanvas(windowWidth, windowHeight);
-
-	// default values
-	xpos = 200;
-	ypos = 200;
-	zpos = 0;
-	AccX = 0;
-	AccY = 0;
-	AccZ = 0;
 }
 
 function draw() 
 {
+  AccBallMove();
 
-  // get accelerometer values
-  AccX = accelerationX;
-  AccY = accelerationY;
-  AccZ = accelerationZ; 
-  
-// // add/subract xpos and ypos
-  xpos = xpos + AccX;
-  ypos = ypos - AccY;
-  emmitAccelerometer(xpos, ypos, zpos);
-	console.log('sendingAccelerometer:', AccX +',', AccY + ',', AccZ);
-	//fill(255, 255, 255); // WHITE drawing local Acc ellipse
-	displayDotAccelerometer(xpos, ypos, zpos);
+  fill(255, 0, 0); // WHITE drawing local Acc ellipse
+	ellipse(myPosX, myPosY, 40);
+
+  emmitAccelerometer(myPosX, myPosY, 0);	
 }
 
-function displayDotAccelerometer(xx, yy, zz)
-{
-	
-	if(xx > windowWidth) {xx = xx % windowWidth; }
-	if(xx < 0) { xx = windowWidth - ((-1 * xx) % windowWidth); }
-	if(yy > windowHeight) { yy = yy % windowHeight; }
-	if(yy < 0) { yy = windowHeight - ((-1 * yy) % windowHeight); }
-	
-	// draw ellipse
-  fill(255, 255, 255); // WHITE drawing local Acc ellipse
-	ellipse(xx, yy, 40);
-}
+function AccBallMove() {
+  myAccX = acelerationX;
+  myAccY = accelerationY;
 
-function displayDotMouse(x, y)
-{
-	ellipse(x, y, 15);
+  myVelX = myVelX + myAccX;
+  myVelY = myVelY + myAccY;
+  myPosY = myPosY + myVelY * vMultiplier;
+  myPosX = myPosX + myVelX * vMultiplier;
+
+  // Bounce when touch the edge of the canvas
+  if (myPosX < 0) {
+    myPosX = 0;
+    myVelX = -myVelX * bMultiplier;
+  }
+  if (myPosY < 0) {
+    myPosY = 0;
+    myVelY = -myVelY * bMultiplier;
+  }
+  if (myPosX > width - 20) {
+    myPosX = width - 20;
+    myVelX = -myVelX * bMultiplier;
+  }
+  if (myPosY > height - 20) {
+    myPosY = height - 20;
+    myVelY = -myVelY * bMultiplier;
+  }
 }
 
 function emmitAccelerometer (INx, INy, INz)
@@ -70,7 +74,9 @@ function emmitAccelerometer (INx, INy, INz)
 		y: INy,
 		z: INz
 	}
-	socket.emit('accelerometer', data);	
+
+	socket.emit('accelerometer', data);
+  console.log('sendingAccelerometer:', INx +',', INy + ',', 0);
 }
 
 function mousePressed()
@@ -81,11 +87,10 @@ function mousePressed()
 function mouseDragged() 
 {	
 	renderer.canvas.style.display = 'block';
-
 	emmitMouse(mouseX, mouseY);
-	console.log('sendingMouse:', mouseX +',', mouseY);
+
 	fill(255, 255, 0); // yellow drawing local Mouse ellipse20
-	displayDotMouse(mouseX, mouseY);
+	ellipse(mouseX, mouseY, 15);
 }
 
 function emmitMouse(INx, INy)
@@ -103,12 +108,12 @@ function ReceivingNewDrawingAcc(data)
 {
 	console.log('receivingAcc :', data.x +', ', data.y + ', ', data.z);
 	fill(255, 0, 0); // red drawing received Acc ellipse
-	displayDotAccelerometer(data.x, data.y, data.z);
+	ellipse(data.x, data.y, 40);
 }
 
 function ReceivingNewDrawingMouse(data)
 {
 	console.log('receivingMouse :', data.x +', ', data.y);
 	fill(0, 255, 255); // cyan drawing received Mouse ellipse
-	displayDotMouse(data.x, data.y);
+	ellipse(data.x, data.y, 40);
 }
