0a9a8d63545806f5a232de04f45e2231a7d4799e
isadorateles <isadoratelesdecastro@gmail.com>
2021-08-27 11:39:56 +0200

    v9 https test for td

--- server.js
+++ server.js
@@ -1,30 +1,53 @@
 
-// NODE SOCKET.IO
-
 'use strict';
 
-var fs = require('fs');
-var https = require('https');
 
-const express = require('express');
-var app = express();
-
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-  };
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
+const INDEX = path.join(__dirname, 'index.html');
 
-var server = https.createServer(options, app);
-var io = require('socket.io')(server);
+var fs = require( 'fs' );
+var https = require('https');
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
-  });
+var app = require('express')();
+var server = https.createServer({ 
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem') 
+ },app);
 
+server.use((req, res) => res.sendFile(INDEX, { root: __dirname }));
 server.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+var io = require('socket.io').listen(server);
+
+////////////////////////////////////////
+// NODE SOCKET.IO
+
+// 'use strict';
+
+// var fs = require('fs');
+// var https = require('https');
+
+// const express = require('express');
+// var app = express();
+
+// const options = {
+//     key: fs.readFileSync('key.pem'),
+//     cert: fs.readFileSync('cert.pem')
+//   };
+
+// const PORT = process.env.PORT || 3000;
+// const INDEX = '/index.html';
+
+// var server = https.createServer(options, app);
+// var io = require('socket.io')(server);
+
+// app.get('/', function(req, res) {
+//     res.sendFile(__dirname + '/public/index.html');
+//   });
+
+// server.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 //////////////////
 
